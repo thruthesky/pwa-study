3e81b1100599be62e40cb47551950f489a30daa6
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-02-03 00:28:39 +0800

    Updating for cache update/delete on activate. Add comment on how to update the service worker.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ PWA 공부하면서 작성한 Repo 입니다.
 * Devtool 에 보시면, Application => service workers 에서 많은 정보를 볼 수 있습니다.
     * Status 의 #??? 와 같이 번호에 따라서 service worker 가 서로 다르게 설치된 것을 볼 수 있습니다.
     * Service worker 를 중단하거나 Unregister 할 수 있습니다.
-    
+
 
 
 # 설명
@@ -29,4 +29,24 @@ PWA 공부하면서 작성한 Repo 입니다.
     cat.png 를 캐시합니다.
 
     이것은 devtool 에서 확인을 할 수 있습니다.
+
+4. service-worker.js 를 수정하면, install 합니다. 하지만, activate 를 시도 하지 않는 것 같습니다. ( 이부분에 대해서는 공부가 필요 함 )
+
+5. caches.open('버전') 에서 버전을 변경하면 "#??? waiting to activate" 라고 나옵니다.
+
+    즉, 변경된 코드를 actviate 하기 위해서 더 노력하는 것 같습니다.
+
+
+# Service Worker Installation and Update
+
+* service worker 소스 코드를 업데이트하면, 
+* 변경하기 전의 기존 service worker 는 계속 실행되고,
+* 새로 변경된 service worker 는 waiting 상태가 된다.
+    * 처음에는 이것 때문에 많이 혼동된다.
+* 하지만, 현재 사용 중인 웹브라우저의 탭 또는 웹브라우저 종료 후에 다시 실행하면, 기존 service worker 는 사라진다.
+    * service worker 의 파일 이름이 동일 한 경우, 헷갈리므로 #??? 와 깉이 service worker ID 번호로 새로운 코드가 적용되었는지 안되었는지 알 수 있다.
+
+# Service Worker 에서 캐시한 데이터 삭제.
+
+'activiate' 이벤트가 발생 할 때, 기존의 캐시 데이터를 삭제 할 수 있다.
 
--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,51 @@
-
+var cacheVersion = "v6";
 
 /**
  * install 이벤트가 fire 되면 무엇을 캐시 할 것인지 결정합니다.
  * 
  * 특이한 점은, 웹 브라우저에서는 cat.png 를 보여주지 않지만, cat.png 을 웹 서버로 부터 가져와서 캐시를 합니다.
  * 
+ * 
+ * 
  */
 self.addEventListener('install', e => {
     
-  console.info('Service workser installation...');
+  console.info('[ServiceWorkser] installation');
   	e.waitUntil(
-        caches.open('cacheName-or-cacheVersion').then(( function(cache) {
-            cache.add('cat.png')
-        }))
+        caches.open( cacheVersion ) // cacheVersion 을 바꿀 때마사 설치를 새로 한다. 단, activate 는 바로 되지 않을 수 있다.
+            .then(( function(cache) {
+                console.info("[ServiceWorker] Going to cache");
+                cache.add('cat.png')
+            })
+        )
     );
     
 });
+
 
 /**
  * Service worker 가 install 된 다음에, activate 이벤트가 fire 됩니다.
  * 
  */
 self.addEventListener('activate', e => {
-    console.info('Your service worker is now activated.');
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        /**
+         * 현재 사용 중인 버전이 아닌 예전 버전(또는 사용중이지 않는 버전)의 모든 캐시 데이터를 삭제한다.
+         */
+        caches.keys().then(function(keyList) {
+            return Promise.all( keyList.map(function(key) {
+                if ( key != cacheVersion ) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+    // 최신 데이터를 리턴하지 않는 경우에, service worker 를 더 빨리 activate 한다고 함.
+    // 참고 : 구글 Your First Progressive Web App 의 final 버젼 service-worker.js 참고.
+    return self.clients.claim();
 });
 
 
@@ -40,11 +63,13 @@ self.addEventListener('activate', e => {
  * 
  */
 self.addEventListener('fetch', event => {
-       const url = new URL(event.request.url);
-  // serve the horse PNG from the cache if the request is
-  // same-origin and the path is '/dog.svg' So same domain
-  if (url.origin == location.origin && url.pathname == '/dog.png') {
-    event.respondWith(caches.match('/horse.png'));
-      }
+    console.log('fetch begins...');
+    const url = new URL(event.request.url);
+    // if ( url.origin == location.origin && url.pathname.indexOf('/dog.png') != -1 ) {
+    //     console.log('hijacking the request and response with cat.png');
+    //     event.respondWith( caches.match('cat.png') );
+    // }
 });
+
+
 
